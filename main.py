d5cb3b56455cf5731a49e7747ad55eec0ddec283
Simon Breneis <simon.breneis@gmx.at>
2021-06-10 18:01:21 +0200

    rBergomi SDE approximation implemented
    Implemented the SDE approximation that was inspired by Alfonsi and Kebaier, with the point set from Harms. For some reason, this approximation does not agree with the method by BFG, and they both do not agree with the results in the BFG paper. Need to find the errors.

--- main.py
+++ main.py
@@ -57,6 +57,12 @@ GK = np.zeros(shape=(N + 1,), dtype=float)
 # GK[1:] = np.array([fractional_kernel_approximation(H, K, float(i)/N) for i in range(1, N+1)])
 
 
+def BlackScholes(T=1., N=1000, sigma=0.2, S_0=1., m=1000):
+    print("The BlackScholes function is deprecated.")
+    normals = np.random.normal(0, 1, size=m)
+    return np.exp(sigma * normals - sigma ** 2 / 2)
+
+
 # noinspection PyShadowingNames
 def rough_Heston_volatility_improved(V_0, theta, lambda_, nu, T, N, W=None):
     """
@@ -265,11 +271,12 @@ def sqrt_cov_matrix_rBergomi_BFG(H=0.1, T=1., N=1000):
     cov = np.empty(shape=(2 * N, 2 * N))
     cov[0:N, 0:N] = scipy.special.gamma(H + 0.5) / scipy.special.gamma(H + 1.5) * np.array([[(np.fmin(i,
                                                                                                       j) * T / N) ** (
-                                                                                                         0.5 + H) * (
-                                                                                                         np.fmax(i,
-                                                                                                                 j) * T / N) ** (
-                                                                                                         H - 0.5) * scipy.special.hyp2f1(
-        0.5 - H, 1., 1.5 + H, np.fmin(float(i), float(j)) / np.fmax(float(i), float(j))) for i in range(1, N + 1)] for j in range(1, N + 1)])
+                                                                                                     0.5 + H) * (
+                                                                                                     np.fmax(i,
+                                                                                                             j) * T / N) ** (
+                                                                                                     H - 0.5) * scipy.special.hyp2f1(
+        0.5 - H, 1., 1.5 + H, np.fmin(float(i), float(j)) / np.fmax(float(i), float(j))) for i in range(1, N + 1)] for j
+                                                                                            in range(1, N + 1)])
     cov[N:(2 * N), N:(2 * N)] = np.array([[np.fmin(i, j) * T / N for i in range(1, N + 1)] for j in range(1, N + 1)])
     cov[0:N, N:(2 * N)] = 1. / (H + 0.5) * np.array(
         [[(i * T / N) ** (H + 0.5) - (np.fmax(i - j, 0) * T / N) ** (H + 0.5) for j in range(1, N + 1)] for i in
@@ -344,7 +351,8 @@ def rBergomi_BFG(H=0.1, T=1., N=1000, eta=1.9, V_0=0.235 ** 2, S_0=1., rho=-0.9,
     S = np.array([S_0 for _ in range(0, m)])
     for i in range(1, N + 1):
         S = S * np.exp(
-            np.sqrt(V[i-1, :]) * (rho * W_diff[i-1, :] + np.sqrt(1-rho**2) * W_2[i-1, :]) - V[i-1, :]*T/(2*N))
+            np.sqrt(V[i - 1, :]) * (rho * W_diff[i - 1, :] + np.sqrt(1 - rho ** 2) * W_2[i - 1, :]) - V[i - 1,
+                                                                                                      :] * T / (2 * N))
     return S
 
 
@@ -367,7 +375,7 @@ def BS_d1(S=1., K=1., sigma=1., r=0., T=1.):
     :param T: Final time
     :return: The first node
     """
-    return (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    return (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
 
 
 def BS_d2(S=1., K=1., sigma=1., r=0., T=1.):
@@ -380,7 +388,7 @@ def BS_d2(S=1., K=1., sigma=1., r=0., T=1.):
     :param T: Final time
     :return: The second node
     """
-    return BS_d1(S, K, sigma, r, T) - sigma*np.sqrt(T)
+    return BS_d1(S, K, sigma, r, T) - sigma * np.sqrt(T)
 
 
 def BS_call_price(S=1., K=1., sigma=1., r=0., T=1.):
@@ -395,7 +403,7 @@ def BS_call_price(S=1., K=1., sigma=1., r=0., T=1.):
     """
     d1 = BS_d1(S, K, sigma, r, T)
     d2 = BS_d2(S, K, sigma, r, T)
-    return S * scipy.stats.norm.cdf(d1) - K * np.exp(-r*T) * scipy.stats.norm.cdf(d2)
+    return S * scipy.stats.norm.cdf(d1) - K * np.exp(-r * T) * scipy.stats.norm.cdf(d2)
 
 
 def BS_put_price(S=1., K=1., sigma=1., r=0., T=1.):
@@ -410,10 +418,10 @@ def BS_put_price(S=1., K=1., sigma=1., r=0., T=1.):
     """
     d1 = BS_d1(S, K, sigma, r, T)
     d2 = BS_d2(S, K, sigma, r, T)
-    return - S * scipy.stats.norm.cdf(-d1) + K * np.exp(-r*T) * scipy.stats.norm.cdf(-d2)
+    return - S * scipy.stats.norm.cdf(-d1) + K * np.exp(-r * T) * scipy.stats.norm.cdf(-d2)
 
 
-def implied_volatility_call(S=1., K=1., r=0., T=1., price=1., tol=10.**(-10), sl=10.**(-10), sr=10.):
+def implied_volatility_call(S=1., K=1., r=0., T=1., price=1., tol=10. ** (-10), sl=10. ** (-10), sr=10.):
     """
     Computes the implied volatility of a European call option, assuming it is in [sl, sr].
     :param S: Initial stock price
@@ -426,12 +434,12 @@ def implied_volatility_call(S=1., K=1., r=0., T=1., price=1., tol=10.**(-10), sl
     :param sr: Right point of the search interval
     :return: The implied volatility
     """
-    sm = (sl + sr)/2
+    sm = (sl + sr) / 2
     while np.amax(sr - sl) > tol:
         em = BS_call_price(S, K, sm, r, T) - price
-        sl = (em < 0)*sm + (em >= 0)*sl
-        sr = (em >= 0)*sm + (em < 0)*sr
-        sm = (sl + sr)/2
+        sl = (em < 0) * sm + (em >= 0) * sl
+        sr = (em >= 0) * sm + (em < 0) * sr
+        sm = (sl + sr) / 2
     return sm
 
 
@@ -486,6 +494,210 @@ def implied_volatility_call_rBergomi_BFG(H=0.1, T=1., N=1000, eta=1.9, V_0=0.235
     :return: The implied volatility and a 95% confidence interval, in the form (estimate, lower, upper)
     """
     samples = rBergomi_BFG(H, T, N, eta, V_0, S_0, rho, m)
+    (price_estimate, price_stat) = MC(call_option_payoff(samples, K))
+    implied_volatility_estimate = implied_volatility_call(S_0, K, r, T, price_estimate)
+    implied_volatility_lower = implied_volatility_call(S_0, K, r, T, price_estimate - price_stat)
+    implied_volatility_upper = implied_volatility_call(S_0, K, r, T, price_estimate + price_stat)
+    return implied_volatility_estimate, implied_volatility_lower, implied_volatility_upper
+
+
+def sqrt_cov_matrix_rBergomi_AK_mpmath(H=0.1, T=1., m=1, n=10, a=1., b=1.):
+    """
+    Computes the Cholesky decomposition of the covariance matrix of one step in the scheme that was inspired by
+    Alfonsi and Kebaier, "Approximation of stochastic Volterra equations with kernels of completely monotone type".
+    More precisely, computes the Cholesky decomposition of the covariance matrix of the following Gaussian vector:
+    (int_0^T exp(-x_1(T-s)) dW_s, ..., int_0^T exp(-x_k(T-s)) dW_s, W_T),
+    where k = m*n is the number of nodes, and these nodes are determined by the m-point quadrature rule approximation
+    of the rough kernel on n subintervals. Note that no weights of the quadrature rule are used. Returns a numpy array,
+    but the computation is done using the mpmath library.
+    :param H: Hurst parameter
+    :param T: Final time, time step size
+    :param m: Order of the quadrature rule
+    :param n: Number of intervals in the quadrature rule
+    :param a: Can shift the left end-point of the total interval of the quadrature rule
+    :param b: Can shift the right end-point of the total interval of the quadrature rule
+    :return: The Cholesky decomposition of the above covariance matrix
+    """
+    mp.mp.dps = int(np.maximum(20., (m ** 5 * n) ** 2))
+    k = m * n
+    quad_rule = quadrature_rule_geometric(H, m, n, a, b)
+    nodes = quad_rule[0, :]
+    weights = quad_rule[1, :]  # weights contain a factor of c_H
+    cov_matrix = mp.matrix(k + 1, k + 1)
+    for i in range(0, n):
+        for j in range(0, n):
+            cov_matrix[i, j] = (1 - mp.exp(-T * (nodes[i] + nodes[j]))) / (
+                    nodes[i] + nodes[j])
+    for i in range(0, n):
+        entry = (1 - mp.exp(-T * nodes[i])) / nodes[i]
+        cov_matrix[n, i] = entry
+        cov_matrix[i, n] = entry
+    cov_matrix[n, n] = T
+    return mp.cholesky(cov_matrix)
+
+
+def sqrt_cov_matrix_rBergomi_AK(H=0.1, T=1., m=1, n=10, a=1., b=1.):
+    """
+    Computes the Cholesky decomposition of the covariance matrix of one step in the scheme that was inspired by
+    Alfonsi and Kebaier, "Approximation of stochastic Volterra equations with kernels of completely monotone type".
+    More precisely, computes the Cholesky decomposition of the covariance matrix of the following Gaussian vector:
+    (int_0^T exp(-x_1(T-s)) dW_s, ..., int_0^T exp(-x_k(T-s)) dW_s, W_T),
+    where k = m*n is the number of nodes, and these nodes are determined by the m-point quadrature rule approximation
+    of the rough kernel on n subintervals. Note that no weights of the quadrature rule are used. Returns a numpy array,
+    but the computation is done using the mpmath library.
+    :param H: Hurst parameter
+    :param T: Final time, time step size
+    :param m: Order of the quadrature rule
+    :param n: Number of intervals in the quadrature rule
+    :param a: Can shift the left end-point of the total interval of the quadrature rule
+    :param b: Can shift the right end-point of the total interval of the quadrature rule
+    :return: The Cholesky decomposition of the above covariance matrix
+    """
+    cov_root_mp = sqrt_cov_matrix_rBergomi_AK_mpmath(H, T, m, n, a, b)
+    num = n * m + 1
+    cov_root = np.empty(shape=(num, num))
+    for i in range(0, num):
+        for j in range(0, num):
+            cov_root[i, j] = float(cov_root_mp[i, j])
+    return cov_root
+
+
+def rBergomi_AK_variance_integral(nodes, weights, t):
+    """
+    Computes an integral that appears in the rBergomi approximation that is inspired by Alfonsi and Kebaier. Computes
+    the integral int_0^t ( sum_i w_i exp(-x_i (t-s)) )^2 ds. Takes a vector in t. May take as an input mpmath elements,
+    but returns a numpy array.
+    :param nodes: The nodes of the quadrature rule
+    :param weights: The weights of the quadrature rule
+    :param t: The current time
+    :return: The vector of integrals
+    """
+    weight_matrix = mp.matrix([[weight_i * weight_j for weight_j in weights] for weight_i in weights])
+    node_matrix = mp.matrix([[node_i + node_j for node_j in nodes] for node_i in nodes])
+    result = np.empty(shape=(len(t),))
+    for time_index in range(len(t)):
+        expression = mp.matrix([[weight_matrix[i, j] / node_matrix[i, j] * (
+                    1 - mp.exp(-node_matrix[i, j] * mp.mpf(t[time_index]))) for j in range(len(nodes))] for i in
+                                range(len(nodes))])
+        expression_np = np.array([[expression[i, j] for j in range(len(nodes))] for i in range(len(nodes))])
+        result[time_index] = np.sum(expression_np.flatten())
+    return result
+
+
+def plot_rBergomi_AK(H=0.1, T=1., N=1000, eta=1.9, V_0=0.235 ** 2, S_0=1., rho=-0.9, m=1, n=10, a=1., b=1.):
+    """
+    Plots a path of the rough Bergomi model together with the variance process (approximation inspired by Alfonsi and
+    Kebaier, with discretization points taken from Harms).
+    :param H: Hurst parameter
+    :param T: Final time
+    :param N: Number of time discretization steps
+    :param eta:
+    :param V_0: Initial variance
+    :param S_0: Initial stock price
+    :param rho: Correlation between the Brownian motions driving the variance process and the stock price
+    :param m: Order of the quadrature rule
+    :param n: Number of intervals in the quadrature rule
+    :param a: Can shift the left end-point of the total interval of the quadrature rule
+    :param b: Can shift the right end-point of the total interval of the quadrature rule
+    :return: Nothing, plots a realization of the stock price and the volatility
+    """
+    dt = T / N
+    quad_rule = quadrature_rule_geometric(H, m, n, a, b)
+    nodes = quad_rule[0, :]
+    weights_mp = quad_rule[1, :]
+    sqrt_cov = sqrt_cov_matrix_rBergomi_AK(H, dt, m, n, a, b)
+    weights = np.array([float(weight) for weight in weights_mp])
+    W_1_diff = sqrt_cov.dot(np.random.normal(0., 1., size=(m * n + 1, N)))
+    eta_transformed = eta * np.sqrt(2 * H) * scipy.special.gamma(
+        H + 0.5)  # the sqrt is in the model, the Gamma takes care of the c_H in the weights of the quadrature rule
+    exp_vector = np.exp(np.array([-float(node) * dt for node in nodes]))
+    W_1_fBm = np.zeros(shape=(m * n, N + 1))
+    for i in range(1, N + 1):
+        W_1_fBm[:, i] = exp_vector * W_1_fBm[:, i - 1] + W_1_diff[:-1, i - 1]
+    W_1_fBm = eta_transformed * np.dot(weights, W_1_fBm)
+    W_1_diff = W_1_diff[-1, :]
+    times = np.array([i * T / N for i in range(0, N + 1)])
+    variances = eta_transformed ** 2 / 2 * rBergomi_AK_variance_integral(nodes, weights, times)
+    V = V_0 * np.exp(W_1_fBm - variances)
+    W_2_diff = np.sqrt(dt) * np.random.normal(0, 1, size=N)
+    S = np.empty(shape=(N + 1,))
+    S[0] = S_0
+    for i in range(1, N + 1):
+        S[i] = S[i - 1] * np.exp(
+            np.sqrt(V[i - 1]) * (rho * W_1_diff[i - 1] + np.sqrt(1 - rho ** 2) * W_2_diff[i - 1]) - V[i - 1] / 2 * dt)
+    plt.plot(times, V)
+    plt.plot(times, S)
+    plt.show()
+
+
+def rBergomi_AK(H=0.1, T=1., N=1000, eta=1.9, V_0=0.235 ** 2, S_0=1., rho=-0.9, m=1, n=10, a=1., b=1., M=1000):
+    """
+    Computes M final stock prices of an approximation of the rough Bergomi model (approximation inspired by Alfonsi and
+    Kebaier, with discretization points taken from Harms).
+    :param H: Hurst parameter
+    :param T: Final time
+    :param N: Number of time discretization steps
+    :param eta:
+    :param V_0: Initial variance
+    :param S_0: Initial stock price
+    :param rho: Correlation between the Brownian motions driving the variance process and the stock price
+    :param m: Order of the quadrature rule
+    :param n: Number of intervals in the quadrature rule
+    :param a: Can shift the left end-point of the total interval of the quadrature rule
+    :param b: Can shift the right end-point of the total interval of the quadrature rule
+    :param M: Number of final stock prices
+    :return: An array containing the final stock prices
+    """
+    dt = T / N
+    quad_rule = quadrature_rule_geometric(H, m, n, a, b)
+    nodes = quad_rule[0, :]
+    weights_mp = quad_rule[1, :]
+    sqrt_cov = sqrt_cov_matrix_rBergomi_AK(H, dt, m, n, a, b)
+    weights = np.array([float(weight) for weight in weights_mp])
+    W_1_diff = np.array([sqrt_cov.dot(np.random.normal(0., 1., size=(m * n + 1, N))) for _ in range(M)])
+    eta_transformed = eta * np.sqrt(2 * H) * scipy.special.gamma(
+        H + 0.5)  # the sqrt is in the model, the Gamma takes care of the c_H in the weights of the quadrature rule
+    exp_vector = np.exp(np.array([-float(node) * dt for node in nodes]))
+    W_1_fBm = np.zeros(shape=(M, m * n, N + 1))
+    for i in range(1, N + 1):
+        W_1_fBm[:, :, i] = exp_vector * W_1_fBm[:, :, i - 1] + W_1_diff[:, :-1, i - 1]
+    for i in range(len(weights)):
+        W_1_fBm[:, i, :] = weights[i] * W_1_fBm[:, i, :]
+    W_1_fBm = eta_transformed * np.sum(W_1_fBm, axis=1)
+    W_1_diff = W_1_diff[:, -1, :]
+    times = np.array([i * T / N for i in range(0, N + 1)])
+    variances = eta_transformed ** 2 / 2 * rBergomi_AK_variance_integral(nodes, weights, times)
+    V = V_0 * np.exp(W_1_fBm - variances)
+    W_2_diff = np.sqrt(dt) * np.random.normal(0, 1, size=(M, N))
+    S = S_0 * np.ones(shape=(M,))
+    for i in range(1, N + 1):
+        S = S * np.exp(
+            np.sqrt(V[:, i - 1]) * (rho * W_1_diff[:, i - 1] + np.sqrt(1 - rho ** 2) * W_2_diff[:, i - 1]) - V[:,
+                                                                                                             i - 1] / 2 * dt)
+    return S
+
+
+def implied_volatility_call_rBergomi_AK(H=0.1, T=1., N=1000, eta=1.9, V_0=0.235 ** 2, S_0=1., rho=-0.9, K=1., m=1, n=10,
+                                        a=1., b=1., M=1000):
+    """
+    Computes the implied volatility of the European call option under the rough Bergomi model, using the approximation
+    inspired by Alfonsi and Kebaier.
+    :param H: Hurst parameter
+    :param T: Final time
+    :param N: Number of time discretization steps
+    :param eta:
+    :param V_0: Initial variance
+    :param S_0: Initial stock price
+    :param rho: Correlation between the Brownian motions driving the volatility and the stock
+    :param K: The strike price
+    :param m: Order of the quadrature rule
+    :param n: Number of intervals in the quadrature rule
+    :param a: Can be used to shift the left endpoint of the total quadrature interval
+    :param b: Can be used to shift the right endpoint of the total quadrature interval
+    :param M: Number of samples
+    :return: The implied volatility and a 95% confidence interval, in the form (estimate, lower, upper)
+    """
+    samples = rBergomi_AK(H, T, N, eta, V_0, S_0, rho, m, n, a, b, M)
     (price_estimate, price_stat) = MC(call_option_payoff(samples, K))
     implied_volatility_estimate = implied_volatility_call(S_0, K, r, T, price_estimate)
     implied_volatility_lower = implied_volatility_call(S_0, K, r, T, price_estimate - price_stat)
@@ -614,23 +826,27 @@ plt.ylabel('Error')
 plt.show()
 '''
 
-sigma_imp = implied_volatility_call(S=100., K=120., r=0.05, T=1., price=2.)
+sigma_imp = implied_volatility_call(S=100., K=120., r=0.05, T=0.5, price=2.)
 print('Test implied volatility: {0}'.format(sigma_imp))
-print('Test call price: {0}'.format(BS_call_price(100., 120., sigma_imp, 0.05, 1.)))
+print('Test call price: {0}'.format(BS_call_price(100., 120., sigma_imp, 0.05, 0.5)))
 
 H = 0.07
 T = 0.9
-N = 100
+N = 1000
 eta = 1.9
-V_0 = 0.235**2
+V_0 = 0.235 ** 2
 S_0 = 1.
 rho = -0.9
 r = 0.
-m = 1000
+M = 30000
 K = 1.
-print('Test implied volatility: {0}'.format(implied_volatility_call_rBergomi_BFG(H, T, N, eta, V_0, S_0, rho, K, m)))
-k_vec = np.array([i/100. for i in range(-20, 13)])
-K_vec = S_0*np.exp(k_vec)
-(mi, lo, up) = implied_volatility_call_rBergomi_BFG(H, T, N, eta, V_0, S_0, rho, K_vec, m)
-plt.plot(k_vec, np.array([mi, lo, up]).transpose())
+m = 1
+n = 20
+a = 1.
+b = 1.
+k_vec = np.array([i / 100. for i in range(-40, 21)])
+K_vec = S_0 * np.exp(k_vec)
+(mi, lo, up) = implied_volatility_call_rBergomi_BFG(H, T, N, eta, V_0, S_0, rho, K_vec, M)
+(mi2, lo2, up2) = implied_volatility_call_rBergomi_AK(H, T, N, eta, V_0, S_0, rho, K_vec, m, n, a, b, M)
+plt.plot(k_vec, np.array([mi, lo, up, mi2, lo2, up2]).transpose())
 plt.show()
