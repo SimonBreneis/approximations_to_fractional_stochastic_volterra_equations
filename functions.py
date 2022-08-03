Bugfix resulting in wrong results when using starting value different from S = 1

functions.py
@@ -264,8 +264,8 @@ def kernel_errors(H, T, Ns=None, modes=None):
             largest_nodes[j, i] = np.amax(nodes)
             kernel_errs[j, i] = np.amax(np.sqrt(rk.error(H=H, nodes=nodes, weights=weights, T=T, output='error'))
                                         / ker_norm)
-            print(f'N={Ns[i]}, mode={modes[j]}, node={largest_nodes[j, i]:.3}, error={100*kernel_errs[j, i]:.4}%, '
-                  + f'time={duration[j, i]:.3}sec')
+            # print(f'N={Ns[i]}, mode={modes[j]}, node={largest_nodes[j, i]:.3}, error={100*kernel_errs[j, i]:.4}%, '
+            #       + f'time={duration[j, i]:.3}sec')
     return largest_nodes, kernel_errs, duration
 
 
@@ -737,8 +737,8 @@ def compute_strong_discretization_errors(Ns=None, N_times=None, N_time_ref=13107
                     S_approx = np.load(filename)
                     errs = np.abs(S - S_approx)**2
                     errors[i, j, k, m], stds[i, j, k, m] = cf.MC(errs)
-    lower = np.sqrt(np.fmax(errors - stats, 0))
-    upper = np.sqrt(errors + stats)
+    lower = np.sqrt(np.fmax(errors - stds, 0))
+    upper = np.sqrt(errors + stds)
     errors = np.sqrt(errors)
 
     def node_line_plots(col_):
@@ -1025,3 +1025,13 @@ def optimize_kernel_approximation_for_simulation_vector_inputs(params, Ns=None, 
     return largest_nodes, true_smile, markov_smiles, approx_smiles, lower_smiles, upper_smiles, total_errors, \
         lower_total_errors, upper_total_errors, markov_errors, discretization_errors, lower_discretization_errors, \
         upper_discretization_errors
+
+
+Hs = np.linspace(0.01, 0.49, 49)
+Ts = np.exp(np.linspace(np.log(0.001), np.log(10), 101))
+Hs, Ts = np.meshgrid(Hs, Ts)
+Hs, Ts = Hs.flatten(), Ts.flatten()
+
+def nasty_parallelization(i):
+
+    return kernel_errors(H=Hs[i], T=Ts[i], Ns=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), modes=None)

rHeston.py
@@ -3,11 +3,12 @@ from scipy.special import gamma
 import rHestonBackbone as backbone
 
 
-def characteristic_function(a, H, lambda_, rho, nu, theta, V_0, T, N_Riccati=200):
+def characteristic_function(a, S, H, lambda_, rho, nu, theta, V_0, T, N_Riccati=200):
     """
     Gives the characteristic function of the log-price in the rough Heston model as described in El Euch and Rosenbaum,
     The characteristic function of rough Heston models. Uses the Adams scheme.
     :param a: Argument of the characteristic function (assumed to be a vector)
+    :param S: Initial stock price
     :param H: Hurst parameter
     :param lambda_: Mean-reversion speed
     :param rho: Correlation between Brownian motions
@@ -42,7 +43,7 @@ def characteristic_function(a, H, lambda_, rho, nu, theta, V_0, T, N_Riccati=200
 
     integral = np.trapz(h, dx=dt)
     fractional_integral = -np.trapz(h, x=np.linspace(T, 0, N_Riccati+1) ** (0.5 - H) / gamma(1.5-H), axis=1)
-    return np.exp(theta * integral + V_0 * fractional_integral)
+    return np.exp(complex(0, 1) * a * np.log(S) + theta * integral + V_0 * fractional_integral)
 
 
 def iv_eur_call(S, K, H, lambda_, rho, nu, theta, V_0, T, rel_tol=1e-03):
@@ -61,6 +62,6 @@ def iv_eur_call(S, K, H, lambda_, rho, nu, theta, V_0, T, rel_tol=1e-03):
     :param rel_tol: Required maximal relative error in the implied volatility
     return: The price of the call option
     """
-    return backbone.iv_eur_call(char_fun=lambda u, T_, N_: characteristic_function(u, H, lambda_, rho, nu, theta, V_0,
-                                                                                   T_, N_),
+    return backbone.iv_eur_call(char_fun=lambda u, T_, N_: characteristic_function(u, S, H, lambda_, rho, nu, theta,
+                                                                                   V_0, T_, N_),
                                 S=S, K=K, T=T, rel_tol=rel_tol)

rHestonMarkov.py
@@ -3,10 +3,11 @@ import RoughKernel as rk
 import rHestonBackbone as backbone
 
 
-def characteristic_function(z, lambda_, rho, nu, theta, V_0, T, N_Riccati, nodes, weights):
+def characteristic_function(z, S, lambda_, rho, nu, theta, V_0, T, N_Riccati, nodes, weights):
     """
     Gives the characteristic function of the log-price in the Markovian approximation of the rough Heston model.
     :param z: Argument of the characteristic function (assumed to be a vector)
+    :param S: Initial stock price
     :param lambda_: Mean-reversion speed
     :param rho: Correlation between Brownian motions
     :param nu: Volatility of volatility
@@ -58,7 +59,7 @@ def characteristic_function(z, lambda_, rho, nu, theta, V_0, T, N_Riccati, nodes
         psi = psi_x @ weights
         F_psi[:, i + 1] = F(psi)
 
-    return np.exp(np.trapz(F_psi * g, dx=dt))
+    return np.exp(complex(0, 1) * z * np.log(S) + np.trapz(F_psi * g, dx=dt))
 
 
 def iv_eur_call(S, K, H, lambda_, rho, nu, theta, V_0, T, N, mode="european", rel_tol=1e-03, nodes=None,
@@ -87,6 +88,6 @@ def iv_eur_call(S, K, H, lambda_, rho, nu, theta, V_0, T, N, mode="european", re
     """
     if nodes is None or weights is None:
         nodes, weights = rk.quadrature_rule(H, N, T, mode)
-    return backbone.iv_eur_call(char_fun=lambda u, T_, N_: characteristic_function(u, lambda_, rho, nu, theta, V_0, T_,
-                                                                                   N_, nodes, weights),
+    return backbone.iv_eur_call(char_fun=lambda u, T_, N_: characteristic_function(u, S, lambda_, rho, nu, theta, V_0,
+                                                                                   T_, N_, nodes, weights),
                                 S=S, K=K, T=T, rel_tol=rel_tol)

rHestonMarkovSamplePaths.py
@@ -152,8 +152,7 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
 
     else:  # Some implicit Euler scheme
         A = np.eye(N) + np.diag(nodes) * dt + lambda_ * weights[None, :] * dt
-        A_inv = np.linalg.inv(A)
-        A_inv_T = A_inv.T
+        A_inv_T = np.linalg.inv(A).T
 
         b_comp = theta * dt + (nodes * V_orig)[None, :] * dt
 
@@ -276,7 +275,7 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
 
         elif vol_behaviour == 'adaptive':
             max_iter = 5
-            A_list = []
+            A_list = [A_inv_T]
             for i in range(1, max_iter + 1):
                 A = np.diag(1 + nodes * (dt / 2 ** i)) + lambda_ * weights[None, :] * (dt / 2 ** i)
                 A_list.append(np.linalg.inv(A).T)
@@ -285,31 +284,32 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
                 n = len(V_comp_loc)
                 if n == 0:
                     return V_comp_loc
-                dW_mid = np.random.normal(dW_loc_ / 2, np.sqrt(dt / 2 ** (iteration + 1)), n)
+                dW_first = np.random.normal(dW_loc_ / 2, np.sqrt(dt / 2 ** (iteration + 1)), n)
 
                 b_ = b_comp / 2 ** iteration + V_comp_loc \
-                    + (nu * np.sqrt(np.fmax(V_comp_loc @ weights, 0)) * dW_mid)[:, None]
-                V_comp_loc_upd = b_ @ A_list[iteration - 1]
+                    + (nu * np.sqrt(np.fmax(V_comp_loc @ weights, 0)) * dW_first)[:, None]
+                V_comp_loc_upd = b_ @ A_list[iteration]
                 V_loc_upd = V_comp_loc_upd @ weights
                 if iteration >= max_iter:
                     # V_comp_loc_upd = V_comp_loc_upd + np.fmax(-V_loc_upd, 0)[:, None] * rescaled_weights[None, :]
                     pass
                 else:
                     crit_ind = V_loc_upd < 0
-                    V_comp_loc_upd[crit_ind] = nested_step(V_comp_loc[crit_ind], dW_mid[crit_ind], iteration + 1)
+                    V_comp_loc_upd[crit_ind, :] = nested_step(V_comp_loc[crit_ind, :], dW_first[crit_ind],
+                                                              iteration + 1)
 
-                dW_second = dW_loc_ - dW_mid
+                dW_second = dW_loc_ - dW_first
                 b_ = b_comp / 2 ** iteration + V_comp_loc_upd + (nu * np.sqrt(
                     np.fmax(V_comp_loc_upd @ weights, 0)) * dW_second)[:, None]
-                V_comp_loc_final = b_ @ A_list[iteration - 1]
+                V_comp_loc_final = b_ @ A_list[iteration]
                 V_loc_final = V_comp_loc_final @ weights
                 if iteration >= max_iter:
                     # V_comp_loc_final = V_comp_loc_final + np.fmax(-V_loc_final, 0)[:, None] * rescaled_weights[None, :]
                     pass
                 else:
                     crit_ind = V_loc_final < 0
-                    V_comp_loc_final[crit_ind] = nested_step(V_comp_loc_upd[crit_ind], dW_second[crit_ind],
-                                                             iteration + 1)
+                    V_comp_loc_final[crit_ind, :] = nested_step(V_comp_loc_upd[crit_ind, :], dW_second[crit_ind],
+                                                                iteration + 1)
                 return V_comp_loc_final
 
             def adaptive(S_, V_, S_BM_, V_comp_, dW_):
@@ -317,7 +317,7 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
                 S_ = S_euler_step(S_, sq_V_, S_BM_)
                 V_comp_2 = V_comp_euler_step(V_comp_, sq_V_, dW_)
                 critical_ind = V_comp_2 @ weights < 0
-                V_comp_2[critical_ind] = nested_step(V_comp_[critical_ind, :], dW_[critical_ind], 1)
+                V_comp_2[critical_ind, :] = nested_step(V_comp_[critical_ind, :], dW_[critical_ind], 1)
                 V_comp_ = V_comp_2
                 V_ = np.fmax(V_comp @ weights, 0)
                 return S_, V_comp_, V_
@@ -352,7 +352,7 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
                 A[j + 1:, :] = 0
                 A = A + np.eye(N)
                 A_invs.append(np.linalg.inv(A).T)
-            A_invs.append(A_inv)
+            A_invs.append(A_inv_T)
             A_inv = A_invs.copy()
 
             def bs(k, vol, dw):
@@ -404,8 +404,7 @@ def sample_values(H, lambda_, rho, nu, theta, V_0, T, S_0, N=None, m=1000, N_tim
                                                     adaptive=True, M=1000000)
 
                     A = np.eye(N) + np.diag(nodes) * dt + lambda_ * (1 + mu_a) * weights[None, :] * dt
-                    A_inv = np.linalg.inv(A)
-                    A_inv_T = A_inv.T
+                    A_inv_T = np.linalg.inv(A).T
             else:
                 N = np.sum(nodes < 2 / dt)
                 nodes = nodes[:N]