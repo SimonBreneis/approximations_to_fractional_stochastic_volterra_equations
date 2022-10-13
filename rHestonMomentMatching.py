e7b1f1fbc6e3a9524c2f0fb2a2e3558c0d5ba72f
Simon Breneis <simon.breneis@gmx.at>
2022-10-13 19:16:05 +0200

    Faster moment computations

--- rHestonMomentMatching.py
+++ rHestonMomentMatching.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 import scipy
 import scipy.integrate
@@ -97,52 +95,35 @@ def cov_V(nodes, weights, lambda_, theta, nu, V_0, dt):
 
 def first_five_moments_V(nodes, weights, lambda_, theta, nu, V_0, dt, rtol=1e-07):
 
-    def compute(n, max_degree=5):
+    def compute():
         """
         Computes the matrices C_ij, up to i <= max_degree.
-        :param n: Number of discretization intervals
-        :param max_degree: Highest degree that should be computed. Is at most 5
         :return: The matrices C_ij
         """
-        N = len(nodes)
-        A = - lambda_ * weights[:, None] - np.diag(nodes)
-
-        psi_1 = np.empty((N, N, n + 1))
-        for i in range(n + 1):
-            psi_1[:, :, i] = scipy.linalg.expm(dt * i / n * A)
         C_11_temp = np.linalg.solve(A, psi_1[:, :, -1] - np.eye(N))
-        if max_degree == 1:
-            return C_11_temp, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52
 
         phi_1 = np.sum(psi_1, axis=0)
         phi_11 = phi_1[:, None, :] * phi_1[None, :, :]
 
-        e_minus_At = np.empty((N, N, n + 1))
-        for i in range(n + 1):
-            e_minus_At[:, :, i] = scipy.linalg.expm(- dt * i / n * A)
         e_minus_At_w = np.einsum('ijk,j-> ik', e_minus_At, weights)
         psi_2 = np.zeros((N, N, N, n + 1))
-        psi_2[:, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, :] * phi_11[None, :, :, :], dx=dt / n, axis=-1)
+        psi_2[:, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, :] * phi_11[None, :, :, :],
+                                                                dx=dt / n, axis=-1)
         psi_2 = np.einsum('ijk,jlmk->ilmk', psi_1, psi_2)
 
         C_21_temp = np.trapz(psi_2, dx=dt / n, axis=-1)
         C_22_temp = nu ** 2 * np.trapz(phi_11, dx=dt / n, axis=-1)
 
-        if max_degree == 2:
-            return C_11_temp, C_21_temp, C_22_temp, C_31, C_32, C_41, C_42, C_51, C_52
-
         phi_2 = np.sum(psi_2, axis=0)
         phi_12 = phi_1[:, None, None, :] * phi_2[None, :, :, :] + phi_1[None, :, None, :] * phi_2[:, None, :, :] \
             + phi_1[None, None, :, :] * phi_2[:, :, None, :]
         psi_3 = np.zeros((N, N, N, N, n + 1))
-        psi_3[:, :, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, None, :] * phi_12[None, :, :, :, :], dx=dt / n, axis=-1)
+        psi_3[:, :, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, None, :]
+                                                                   * phi_12[None, :, :, :, :], dx=dt / n, axis=-1)
         psi_3 = np.einsum('ijk,j...k->i...k', psi_1, psi_3)
 
         C_31_temp = np.trapz(psi_3, dx=dt / n, axis=-1)
         C_32_temp = nu ** 2 * np.trapz(phi_12, dx=dt / n, axis=-1)
-
-        if max_degree == 3:
-            return C_11_temp, C_21_temp, C_22_temp, C_31_temp, C_32_temp, C_41, C_42, C_51, C_52
 
         phi_3 = np.sum(psi_3, axis=0)
         phi_13 = phi_1[:, None, None, None, :] * phi_3[None, :, :, :, :] \
@@ -155,14 +136,13 @@ def first_five_moments_V(nodes, weights, lambda_, theta, nu, V_0, dt, rtol=1e-07
         phi_13_22 = phi_13 + phi_22
 
         psi_4 = np.zeros((N, N, N, N, N, n + 1))
-        psi_4[:, :, :, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, None, None, :] * phi_13_22[None, :, :, :, :, :], dx=dt / n, axis=-1)
+        psi_4[:, :, :, :, :, 1:] = nu ** 2 * scipy.integrate.cumtrapz(e_minus_At_w[:, None, None, None, None, :]
+                                                                      * phi_13_22[None, :, :, :, :, :], dx=dt / n,
+                                                                      axis=-1)
         psi_4 = np.einsum('ijk,j...k->i...k', psi_1, psi_4)
 
         C_41_temp = np.trapz(psi_4, dx=dt / n, axis=-1)
         C_42_temp = nu ** 2 * np.trapz(phi_13_22, dx=dt / n, axis=-1)
-
-        if max_degree == 4:
-            return C_11_temp, C_21_temp, C_22_temp, C_31_temp, C_32_temp, C_41_temp, C_42_temp, C_51, C_52
 
         phi_4 = np.sum(psi_4, axis=1)
         phi_14 = phi_1[:, None, None, None, None, :] * phi_4[None, :, :, :, :, :] \
@@ -193,16 +173,36 @@ def first_five_moments_V(nodes, weights, lambda_, theta, nu, V_0, dt, rtol=1e-07
         return C_11_temp, C_21_temp, C_22_temp, C_31_temp, C_32_temp, C_41_temp, C_42_temp, C_51_temp, C_52_temp
 
     n = 64
-    C_11, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52 = compute(n, 5)
+    N = len(nodes)
+    A = - lambda_ * weights[:, None] - np.diag(nodes)
+    psi_1 = np.empty((N, N, n + 1))
+    for i in range(n + 1):
+        psi_1[:, :, i] = scipy.linalg.expm(dt * i / n * A)
+
+    e_minus_At = np.empty((N, N, n + 1))
+    for i in range(n + 1):
+        e_minus_At[:, :, i] = scipy.linalg.expm(- dt * i / n * A)
+
+    def refine():
+        e_A_ddt = scipy.linalg.expm(dt / n * A)
+        psi_1_ = np.empty((N, N, n + 1))
+        psi_1_[:, :, ::2] = psi_1
+        psi_1_[:, :, 1::2] = np.einsum('ij,jkl->ikl', e_A_ddt, psi_1[:, :, :-1])
+        e_minus_At_ = np.empty((N, N, n + 1))
+        e_minus_At_[:, :, ::2] = e_minus_At
+        e_minus_At_[:, :, 1::2] = np.einsum('ij,jkl->ikl', e_A_ddt, e_minus_At[:, :, 1:])
+        return psi_1_, e_minus_At_
+
+    C_11, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52 = compute()
     error = 10 * rtol * np.ones(5)
 
     while np.amax(error) > rtol:
         n = 2 * n
-        highest_degree = (error > rtol).nonzero()[0][-1] + 1
-        print(error, highest_degree, n)
+        print(error, n)
         C_11_old, C_21_old, C_22_old, C_31_old, C_32_old, C_41_old, C_42_old, C_51_old, C_52_old = C_11, C_21, C_22, \
             C_31, C_32, C_41, C_42, C_51, C_52
-        C_11, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52 = compute(n, highest_degree)
+        psi_1, e_minus_At = refine()
+        C_11, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52 = compute()
         error[0] = np.amax(np.abs((C_11_old - C_11) / C_11))
         error[1] = np.fmax(np.amax(np.abs((C_21_old - C_21) / C_21)), np.amax(np.abs((C_22_old - C_22) / C_22)))
         error[2] = np.fmax(np.amax(np.abs((C_31_old - C_31) / C_31)), np.amax(np.abs((C_32_old - C_32) / C_32)))
@@ -230,8 +230,8 @@ def first_five_moments_V(nodes, weights, lambda_, theta, nu, V_0, dt, rtol=1e-07
         f_111 = f_11[:, :, None] * f_1[None, None, :]
         f_13 = f_1[:, None, None, None] * f_3[None, :, :, :] + f_1[None, :, None, None] * f_3[:, None, :, :] \
             + f_1[None, None, :, None] * f_3[:, :, None, :] + f_1[None, None, None, :] * f_3[:, :, :, None]
-        f_22 = f_2[:, :, None, None] * f_2[None, None, :, :] + f_2[:, None, :, None] * f_2[None, :, None, :] \
-            + f_2[:, None, None, :] * f_2[None, :, :, None]
+        f_22_4 = f_2[:, :, None, None] * f_2[None, None, :, :] + f_2[:, None, :, None] * f_2[None, :, None, :] \
+            + f_2[:, None, None, :] * f_2[None, :, :, None] + f_4
         f_112 = f_11[:, :, None, None] * f_2[None, None, :, :] + f_11[:, None, :, None] * f_2[None, :, None, :] \
             + f_11[:, None, None, :] * f_2[None, :, :, None] + f_11[None, :, :, None] * f_2[:, None, None, :] \
             + f_11[None, :, None, :] * f_2[:, None, :, None] + f_11[None, None, :, :] * f_2[:, :, None, None]
@@ -246,46 +246,19 @@ def first_five_moments_V(nodes, weights, lambda_, theta, nu, V_0, dt, rtol=1e-07
             + f_2[None, None, :, :, None] * f_3[:, :, None, None, :] \
             + f_2[None, None, :, None, :] * f_3[:, :, None, :, None] \
             + f_2[None, None, None, :, :] * f_3[:, :, :, None, None]
-        f_14 = f_1[:, None, None, None, None] * f_4[None, :, :, :, :] \
-            + f_1[None, :, None, None, None] * f_4[:, None, :, :, :] \
-            + f_1[None, None, :, None, None] * f_4[:, :, None, :, :] \
-            + f_1[None, None, None, :, None] * f_4[:, :, :, None, :] \
-            + f_1[None, None, None, None, :] * f_4[:, :, :, :, None]
-        f_122 = f_1[:, None, None, None, None] * f_22[None, :, :, :, :] \
-            + f_1[None, :, None, None, None] * f_22[:, None, :, :, :] \
-            + f_1[None, None, :, None, None] * f_22[:, :, None, :, :] \
-            + f_1[None, None, None, :, None] * f_22[:, :, :, None, :] \
-            + f_1[None, None, None, None, :] * f_22[:, :, :, :, None]
-        f_113 = f_11[:, :, None, None, None] * f_3[None, None, :, :, :] \
-            + f_11[:, None, :, None, None] * f_3[None, :, None, :, :] \
-            + f_11[:, None, None, :, None] * f_3[None, :, :, None, :] \
-            + f_11[:, None, None, None, :] * f_3[None, :, :, :, None] \
-            + f_11[None, :, :, None, None] * f_3[:, None, None, :, :] \
-            + f_11[None, :, None, :, None] * f_3[:, None, :, None, :] \
-            + f_11[None, :, None, None, :] * f_3[:, None, :, :, None] \
-            + f_11[None, None, :, :, None] * f_3[:, :, None, None, :] \
-            + f_11[None, None, :, None, :] * f_3[:, :, None, :, None] \
-            + f_11[None, None, None, :, :] * f_3[:, :, :, None, None]
-        f_1112 = f_111[:, :, :, None, None] * f_2[None, None, None, :, :] \
-            + f_111[:, :, None, :, None] * f_2[None, None, :, None, :] \
-            + f_111[:, :, None, None, :] * f_2[None, None, :, :, None] \
-            + f_111[:, None, :, :, None] * f_2[None, :, None, None, :] \
-            + f_111[:, None, :, None, :] * f_2[None, :, None, :, None] \
-            + f_111[:, None, None, :, :] * f_2[None, :, :, None, None] \
-            + f_111[None, :, :, :, None] * f_2[:, None, None, None, :] \
-            + f_111[None, :, :, None, :] * f_2[:, None, None, :, None] \
-            + f_111[None, :, None, :, :] * f_2[:, None, :, None, None] \
-            + f_111[None, None, :, :, :] * f_2[:, :, None, None, None]
-        f_11111 = f_1[:, None, None, None, None] * f_1111[None, :, :, :, :]
+        temp = f_22_4 + f_13 / 2 + f_112 / 3 + f_1111 / 5
+        f_11111_1112_122_113_14 = f_1[:, None, None, None, None] * temp[None, :, :, :, :] \
+            + f_1[None, :, None, None, None] * temp[:, None, :, :, :] \
+            + f_1[None, None, :, None, None] * temp[:, :, None, :, :] \
+            + f_1[None, None, None, :, None] * temp[:, :, :, None, :] \
+            + f_1[None, None, None, None, :] * temp[:, :, :, :, None]
 
         g_1 = f_1
         g_2 = f_2 + f_11
         g_3 = f_3 + f_12 + f_111
-        g_4 = f_4 + f_13 + f_22 + f_112 + f_1111
-        g_5 = f_5 + f_23 + f_14 + f_122 + f_113 + f_1112 + f_11111
+        g_4 = f_13 + f_22_4 + f_112 + f_1111
+        g_5 = f_5 + f_23 + f_11111_1112_122_113_14
 
         return g_1, g_2, g_3, g_4, g_5
 
-    # return C_11, C_21, C_22, C_31, C_32, C_41, C_42, C_51, C_52
     return moments
-
